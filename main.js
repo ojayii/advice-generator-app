5eb99466134e092582da59569b8de2664adcfc34
Joseph Ogelenya <josephoges9@gmail.com>
2024-01-19 02:57:53 +0100

    Added all design and functionalities

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-console.log("Hello, world!");
+let adviceId = document.querySelector(".advice-id span");
+let adviceText = document.querySelector(".advice-text");
+let diceBtn = document.querySelector(".dice");
 
 const myTest = () => {
     const xhttp = new XMLHttpRequest();
 
     xhttp.onloadstart = () => {
-        console.log("loading...")
+        diceBtn.disable;
+        diceBtn.style.backgroundColor = "hsl(193, 38%, 86%)";
+        // diceBtn.style.boxShadow = "0 0 2rem hsl(193, 38%, 86%)";
     }
-    
+
     xhttp.onload = () => {
-        document.write(JSON.parse(xhttp.responseText).slip.advice)
-        console.log(xhttp)
+        adviceId.innerHTML = JSON.parse(xhttp.responseText).slip.id;
+        adviceText.innerHTML = `"${JSON.parse(xhttp.responseText).slip.advice}"`;
     }
 
     xhttp.onloadend = () => {
-        console.log("done!")
+        diceBtn.enable;
+        diceBtn.style.backgroundColor = "";
     }
-
 
     xhttp.open("GET", "https://api.adviceslip.com/advice");
     xhttp.send()
 }
 
 myTest();
+
+diceBtn.addEventListener("click", myTest)
